f8a4a6e407651b2d7ebdeec865b93abd96e52d94
Steven Inouye <steveninouye@msn.com>
2018-06-12 16:43:18 -1000

    keys hidden

--- src/component/Search/search-lists/search-video.js
+++ src/component/Search/search-lists/search-video.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+import apiKey from '../../../../config/YTkey';
 
 class SearchVideo extends Component {
   render() {
+    YTSearch({ key: apiKey, term: 'surfboards' }, data => console.log(data));
     return <div>Video is here</div>;
   }
 }
